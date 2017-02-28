8b4a1edff413023f3139e114757205b2a5ce7b1b
vstanyshevskyy <stvitaliyvskiy@gmail.com>
2017-02-28 12:22:12 +0200

    Changed task update

--- bin/ca-task.js
+++ bin/ca-task.js
@@ -2,19 +2,16 @@
 const program = require('commander')
 const currentStory = require('current-story')
 const getEmail = require('git-user-email')
-const _ = require('lodash')
-const repoName = require('git-repo-name')
-const rally = require('rally')
 const printTasks = require('../lib/print-tasks')
 const getBacklogItem = require('../lib/get-backlog-item')
 const createTask = require('../lib/create-task')
-const getState = require('../lib/task-state-mapper')
-const getUserByEmail = require('../lib/get-user-by-email')
 const updateTask = require('../lib/update-task')
 const getArtifact = require('../lib/get-artifact')
 const promtForArtifactId = require('../lib/promt-for-artifact-id')
 const promtForTaskInfo = require('../lib/promt-for-task-info')
+const getArtifactWithTasks = require('../lib/get-artifact-with-tasks')
 const inquirer = require('inquirer')
+require('loud-rejection')()
 
 program
   .command('list')
@@ -70,65 +67,61 @@ program
   })
 
 program
-  .command('new-merge [taskPrefix]')
-  .description('Adds a new merge task to a story/defect')
-  .action(taskPrefix => {
-    const email = getEmail()
-
-    function createMergeTask (artifact, name) {
-      getUserByEmail(email).then((user) => {
-        return createTask({
-          name: name,
-          projectRef: artifact.Project._ref,
-          artifactRef: artifact._ref,
-          userRef: user._ref,
-          fetch: ['FormattedID', 'Name']
-        })
-      }).then((taskInfo) => {
-        console.log('Merge Task created for you to not forget:')
-        console.log(taskInfo.Object.FormattedID + '. ' + taskInfo.Object.Name)
-      })
-    }
-
-    const taskName = taskPrefix + ' ' + repoName.sync()
-
-    currentStory().then((itemId) => {
-      if (!itemId) {
-        console.log('item not found. terminating')
-        return
-      }
-      return getBacklogItem(itemId).then((artifact) => {
-        printTasks().then(tasks => {
-          if (_.find(tasks, ['Name', taskName])) {
-            console.log('Merge task already exists. Lucky you!')
-            return
-          }
-          createMergeTask(artifact, taskName)
-        })
-      })
-    })
-  })
-
-program
-  .command('update [formattedId] [status] [actuals]')
+  .command('update')
   .description('Updates status and actuals of the task')
-  .action((formattedId, state, actuals) => {
-    const queryUtils = rally.util.query
-
-    getArtifact({
-      type: 'task',
-      query: queryUtils.where('FormattedID', '=', formattedId)
-    }).then(tasks => {
-      let data = {
-        ref: tasks.Results[0]._ref
-      }
-      if (state && typeof state === 'string') {
-        data.state = getState(state)
-      }
-      if (actuals) {
-        data.actuals = parseFloat(actuals)
-      }
-      updateTask(data)
+  .action(() => {
+    let email
+    let artifact
+    let users
+    let task
+    Promise.all([currentStory(), getEmail()]).then((values) => {
+      email = values[1]
+      return inquirer.prompt([promtForArtifactId(values[0])])
+    }).then(answers => {
+      return getArtifactWithTasks(answers.id)
+    }).then(artifactWithTasks => {
+      artifact = artifactWithTasks
+      return getArtifact({
+        type: 'TeamMembers',
+        ref: artifact.Project.TeamMembers._ref,
+        fetch: ['UserName', 'FirstName', 'MiddleName', 'LastName', 'DisplayName', 'EmailAddress']
+      })
+    }).then(usersData => {
+      users = usersData.Results
+      // defaults.owner || defaults.users.indexOf(defaults.users.find(u => u.EmailAddress === defaults.email))
+      return inquirer.prompt([{
+        type: 'list',
+        name: '_ref',
+        message: 'Select a task:',
+        default: artifact.tasks[0],
+        choices: artifact.tasks.map(t => {
+          return {
+            name: `${t.FormattedID}. ${t.Name} - ${t.Owner.DisplayName}`,
+            value: t._ref
+          }
+        })
+      }])
+    }).then(taskChoice => {
+      task = artifact.tasks.find(t => { return t._ref === taskChoice._ref })
+      return inquirer.prompt(promtForTaskInfo({
+        users: users,
+        email: email,
+        name: task.Name,
+        owner: users.indexOf(users.find(u => u.EmailAddress === task.Owner.UserName)),
+        stateIndex: ['Defined', 'In-Progress', 'Completed'].indexOf(task.State),
+        estimate: task.Estimate,
+        actuals: task.Actuals
+      }))
+    }).then(answers => {
+      return updateTask({
+        ref: task._ref,
+        userRef: answers.owner,
+        state: answers.state,
+        estimate: answers.estimate,
+        actuals: answers.actuals
+      })
+    }).then(info => {
+      printTasks(artifact.FormattedID)
     })
   })
 
